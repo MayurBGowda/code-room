2bfba91cdd2bb606fa603a218d51c4cba6888c2c
Mayur Gowda <mayurgowda059@gmail.com>
2026-06-22 17:18:22 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+console.log(ENV.PORT);
+
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "success from api 123" });
+});
+
+app.listen(ENV.PORT, () => console.log("Server is running on port :",ENV.PORT));
